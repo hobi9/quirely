3f0b7ab3833db8b6f7c51902255bc3630eea0344
H. <127121294+hobi9@users.noreply.github.com>
2025-01-25 23:17:07 +0100

    FE - adds last migration step from tailwind v3 to v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,11 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
