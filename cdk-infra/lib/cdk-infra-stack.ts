e55f43b06fbe77b9e402bc9241028201c1048c4e
H <127121294+hobi9@users.noreply.github.com>
2025-07-19 19:53:53 +0200

    Revert "Import Stack from aws-cdk-lib and assign to variable"
    This reverts commit 081b9518d9bcca557de582ede4ab579ce245f8ce.

--- cdk-infra/lib/cdk-infra-stack.ts
+++ cdk-infra/lib/cdk-infra-stack.ts
@@ -11,7 +11,6 @@ import * as ses from "aws-cdk-lib/aws-ses";
 import * as elasticbeanstalk from "aws-cdk-lib/aws-elasticbeanstalk";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
-import { Stack } from "aws-cdk-lib";
 
 export class CdkInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -24,8 +23,6 @@ export class CdkInfraStack extends cdk.Stack {
         description: "Comma-separated list of CloudFront IP ranges",
       },
     );
-
-    const stack = Stack.of(this);
 
     const stageNameParam = new cdk.CfnParameter(this, "stageName", {
       type: "String",
